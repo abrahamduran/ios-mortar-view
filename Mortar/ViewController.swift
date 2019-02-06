e55ee95a5a5a56107a99a7e76d91daf207c8e3d8
Abraham Isaac Durán <aid.mest@gmail.com>
2019-02-06 14:48:03 -0400

    Initial Commit

---
+++ Mortar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mortar
+//
+//  Created by Abraham Isaac Durán on 2/6/19.
+//  Copyright © 2019 Abraham Isaac Durán. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
